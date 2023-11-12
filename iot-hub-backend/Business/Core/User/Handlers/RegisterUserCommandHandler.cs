65af6024b88070b7f748b5dfe42662dfbeae8260
Mateusz Cholewa <mateusz.cholewa@proton.me>
2023-11-12 21:41:37 +0100

    Usunieto WeatherForecastController. closes #14
    Wypelniono RegisterUserCommandHandler

--- iot-hub-backend/Business/Core/User/Handlers/RegisterUserCommandHandler.cs
+++ iot-hub-backend/Business/Core/User/Handlers/RegisterUserCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Core.User.Commands;
+using Business.Infrastructure.Security;
+using Domain.Data;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,30 @@ namespace Business.Core.User.Handlers
 {
     public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserCommandResult>
     {
-        public Task<RegisterUserCommandResult> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        private readonly IoTHubContext _context;
+        private readonly Business.Infrastructure.Security.IPasswordHasher _passHasher;
+
+        public RegisterUserCommandHandler(IoTHubContext context, IPasswordHasher passHasher)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _passHasher = passHasher;
+        }
+
+        public async Task<RegisterUserCommandResult> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+
+            if(request.Password == null || request.Email == null)
+            {
+                return new RegisterUserCommandResult() { IsSuccess = false };
+            }
+
+            string pwdHash = _passHasher.HashPassword(request.Password);
+            var user = new Domain.Core.User { Email = request.Email, PasswordHash = pwdHash };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            return new RegisterUserCommandResult() { IsSuccess = true, ResultUser = user };
         }
     }
 }
