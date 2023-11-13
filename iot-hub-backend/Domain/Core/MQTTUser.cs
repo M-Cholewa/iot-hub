2338832e5ffb0d9898bc48d98e5f71f2a2cb0766
Mateusz Cholewa <mateusz.cholewa@proton.me>
2023-11-13 23:14:25 +0100

    schowano pola z haslami w wysylanych modelach

--- iot-hub-backend/Domain/Core/MQTTUser.cs
+++ iot-hub-backend/Domain/Core/MQTTUser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Core
@@ -11,6 +12,7 @@ namespace Domain.Core
     {
         public Guid ClientID { get; set; } // ClientID
         public string? Username { get; set; }
+        [JsonIgnore]
         public string? PasswordHash { get; set; }
     }
 }
--- iot-hub-backend/Domain/Core/User.cs
+++ iot-hub-backend/Domain/Core/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Core
@@ -14,6 +15,7 @@ namespace Domain.Core
 
         public string? Email { get; set; }
 
+        [JsonIgnore]
         public string? PasswordHash { get; set; }
 
         public virtual ICollection<Role>? Roles { get; set; }
