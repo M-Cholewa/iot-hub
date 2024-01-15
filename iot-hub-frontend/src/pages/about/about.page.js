8ae9297a04900ba2d12d7a55875e64d84082389e
Mateusz Cholewa <mateusz.cholewa@proton.me>
2024-01-15 19:21:47 +0100

    usuniete niepotrzebne wersy z /about

--- iot-hub-frontend/src/pages/about/about.page.js
+++ iot-hub-frontend/src/pages/about/about.page.js
@@ -8,16 +8,6 @@ export const AboutPage = () => {
                 <Typography variant="h5" mt={2} >
                     O Projekcie IoThub
                 </Typography>
-                <Typography variant="body1" ml={2} >
-                    Witaj na stronie projektu IoThub! Jestem studentem Politechniki Krakowskiej, który poświęcił swój czas i wysiłek na stworzenie tego innowacyjnego systemu zarządzania Internetem Rzeczy (IoT).
-                </Typography>
-
-                <Typography variant="h6" mt={1} >
-                    Kim Jestem
-                </Typography>
-                <Typography variant="body1" ml={2} >
-                    Jestem studentem Politechniki Krakowskiej, pasjonatem technologii i entuzjastą rozwoju projektów z zakresu Internetu Rzeczy. IoThub to efekt mojej pracy nad projektem edukacyjnym, mającym na celu zdobycie praktycznych umiejętności w obszarze programowania i inżynierii.
-                </Typography>
 
                 <Typography variant="h6" mt={1} >
                     IoThub - System Zarządzania IoT
@@ -34,12 +24,6 @@ export const AboutPage = () => {
                     <li>Zdalna Kontrola: Umożliwiam zdalne sterowanie połączonymi urządzeniami, co pozwala na interakcję i zarządzanie nimi z dowolnego miejsca na świecie.</li>
                     <li>Bezpieczeństwo: Bezpieczeństwo danych i urządzeń jest dla mnie priorytetem. IoThub implementuje zaawansowane mechanizmy ochrony, zapewniające poufność i integralność danych.</li>
                 </ul>
-                <Typography variant="h6" mt={1} >
-                    Dlaczego IoThub?
-                </Typography>
-                <Typography variant="body1" ml={2} >
-                    IoThub został stworzony z myślą o dostarczeniu kompleksowego i elastycznego rozwiązania dla zarządzania Internetem Rzeczy. Projekt ten nie tylko spełnia wymagania edukacyjne, ale również stanowi praktyczny wkład w rozwój technologii IoT.
-                </Typography>
 
                 <Typography variant="h6" mt={1} >
                     Kontakt
