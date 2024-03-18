ea6c5887d8c8dcac6c786b3f1623611730fdd9e9
Mateusz Cholewa <mateusz.cholewa@proton.me>
2024-03-18 21:46:58 +0100

    Dodana data do logów na stronie głównej. Closes #50

--- iot-hub-frontend/src/pages/dashboard/lastNotifications.js
+++ iot-hub-frontend/src/pages/dashboard/lastNotifications.js
@@ -1,15 +1,24 @@
-import { Alert, Stack } from '@mui/material';
+import { Alert, Stack, Typography } from '@mui/material';
 
 export const LastNotifications = () => {
     return (
         <Stack spacing={1}>
-            <Alert variant="outlined" severity="info">
+            <Alert variant="outlined" severity="info"
+                action={
+                    <Typography variant='span'>18 Mar 2024 19:40:21</Typography>
+                }>
                 ESP32-1: Device connected.
             </Alert>
-            <Alert variant="outlined" severity="warning">
+            <Alert variant="outlined" severity="warning"
+                action={
+                    <Typography variant='span'>18 Mar 2024 19:40:21</Typography>
+                }>
                 ESP32-2: Low battery level (5%).
             </Alert>
-            <Alert variant="outlined" severity="error">
+            <Alert variant="outlined" severity="error"
+                action={
+                    <Typography variant='span'>18 Mar 2024 19:40:21</Typography>
+                }>
                 ESP32-2: No data from temperature sensor.
             </Alert>
         </Stack>
